d608da8b5c9b2a46513dedbfa066dfc6b06978ec
Jordan Lafland <lafland.design@gmail.com>
2015-04-16 20:56:07 -0400

    test anchor scroll

--- wevue-angular/js/app.js
+++ wevue-angular/js/app.js
@@ -36,3 +36,15 @@ angular
         controller: 'enhanceCtrl'
       });
   }]);
+
+  .controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+  function ($scope, $location, $anchorScroll) {
+    $scope.gotoTop = function() {
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash('top');
+
+      // call $anchorScroll()
+      $anchorScroll();
+    };
+  }]);
