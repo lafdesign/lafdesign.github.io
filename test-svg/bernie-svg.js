4a392e1bcab2210de7ac1b585def2725b9efcc63
Jordan Lafland <jordan.github@gmail.com>
2015-12-12 14:07:32 -0500

    add new svg files

---
+++ test-svg/bernie-svg.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.onload = function () {
+  var svgMorpheus = new SVGMorpheus('#icon', [700]),
+
+
+      icons={
+
+        'Bernie':'Bernie',
+      
+        'White-House':'White-House',
+    
+      }}
+  
