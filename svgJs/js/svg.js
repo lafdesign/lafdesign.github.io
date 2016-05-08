697b84fc7a28e157e26d8fef9bde4023db2d3134
jordan lafland <jordan.lafland@fcb.com>
2016-05-08 16:33:06 -0400

    replace viewbox and x y coodinates with variables

--- svgJs/js/svg.js
+++ svgJs/js/svg.js
@@ -1,29 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
 
 
-var reset = document.getElementsByTagName('body')[0];
+
+var rangeX  = '100';
+var viewBox = 0 + " " + 0 + " " + rangeX + " " + rangeX;
 
 
-reset.style.margin = "0"; 
-reset.style.padding = "0"; 
 
+var body = document.getElementsByTagName('body')[0];
+body.style.margin  = "0"; 
+body.style.padding = "0"; 
 
+var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg'); 
+body.appendChild(svg);
 
 var svg = document.getElementsByTagName('svg')[0]; //Get svg element
+svg.setAttribute("viewBox"  ,  viewBox  ); 
 
 var square = document.createElementNS("http://www.w3.org/2000/svg", 'rect'); //Create a path in SVG's namespace
-square.setAttribute("width","100"); 
-square.setAttribute("height","100");
-square.setAttribute("stroke", "#000"); //Set stroke colour
-square.setAttribute("strokeWidth", "3");
+square.setAttribute("width"  , rangeX   ); 
+square.setAttribute("height" , rangeX   );
+square.setAttribute("stroke" , "#000"   );  
+square.setAttribute("strokeWidth" , "3" );
 
 
 var circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle'); //Create a path in SVG's namespace
-circle.setAttribute("cx","50"); 
-circle.setAttribute("cy","50");
-circle.setAttribute("r","50");
-circle.style.stroke = "#000"; //Set stroke colour
-circle.style.strokeWidth = "3"; //Set stroke width
-circle.style.fill = "#565656"; //Set fill
+circle.setAttribute("cx" , rangeX/2 ); 
+circle.setAttribute("cy" , rangeX/2 );
+circle.setAttribute("r"  , rangeX/2 );
+circle.style.stroke = "#000"; 
+circle.style.strokeWidth = "3"; 
+circle.style.fill = "#565656"; 
 circle.style.position = "absolute"; 
 
 
@@ -31,3 +38,4 @@ svg.appendChild(square);
 svg.appendChild(circle);
 
 
+});
