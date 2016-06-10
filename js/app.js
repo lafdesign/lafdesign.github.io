3cdca80f5d2ec66afb9d83f6ed303854b55737ad
Jordan Lafland <jordan.github@gmail.com>
2016-06-09 21:34:01 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -44,18 +44,7 @@ angular
         url : '/'
       });
       
-      if(window.history && window.history.pushState){
-            //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
-
-         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
-
-         // if you don't wish to set base URL then use this
-         $locationProvider.html5Mode({
-                 enabled: true,
-                 requireBase: false
-          });
-        }
-
+    $locationProvider.html5Mode(true);// use the HTML5 History API -- removes the #
       
   }])
 
